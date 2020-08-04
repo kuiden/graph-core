b1a1c3bd3bf2415cd8fc2dcc338beb15f5eb2b7d
kudeng <kudeng@tuhu.cn>
2020-08-04 17:19:47 +0800

    营销活动数据库mapper添加basemapper

--- store-saas-marketing-integration-dal/src/main/java/com/tuhu/store/saas/marketing/mysql/marketing/write/dao/ActivityTemplateMapper.java
+++ store-saas-marketing-integration-dal/src/main/java/com/tuhu/store/saas/marketing/mysql/marketing/write/dao/ActivityTemplateMapper.java
@@ -1,5 +1,6 @@
 package com.tuhu.store.saas.marketing.mysql.marketing.write.dao;
 
+import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.tuhu.store.saas.marketing.po.ActivityTemplate;
 import com.tuhu.store.saas.marketing.po.ActivityTemplateExample;
 import org.apache.ibatis.annotations.Mapper;
@@ -8,14 +9,14 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 @Mapper
-public interface ActivityTemplateMapper {
+public interface ActivityTemplateMapper extends BaseMapper<ActivityTemplate> {
     int countByExample(ActivityTemplateExample example);
 
     int deleteByExample(ActivityTemplateExample example);
 
     int deleteByPrimaryKey(Long id);
 
-    int insert(ActivityTemplate record);
+    Integer insert(ActivityTemplate record);
 
     int insertSelective(ActivityTemplate record);
 
