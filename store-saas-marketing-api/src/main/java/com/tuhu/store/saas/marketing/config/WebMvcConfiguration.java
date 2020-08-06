349ab57c7e00aaf3912644011610d4f938b80546
wangyuqing <wangyuqing@tuhu.cn>
2020-08-06 19:57:32 +0800

    merge拦截器配置

--- store-saas-marketing-api/src/main/java/com/tuhu/store/saas/marketing/config/WebMvcConfiguration.java
+++ store-saas-marketing-api/src/main/java/com/tuhu/store/saas/marketing/config/WebMvcConfiguration.java
@@ -1,5 +1,7 @@
 package com.tuhu.store.saas.marketing.config;
 
+import com.tuhu.store.saas.marketing.interceptor.CustomerInterceptor;
+import com.tuhu.store.saas.marketing.interceptor.EndUserInterceptor;
 import com.tuhu.store.saas.marketing.interceptor.UserInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -18,13 +20,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Autowired
     private EndUserInterceptor endUserInterceptor;
 	
-	   @Autowired
+    @Autowired
     private CustomerInterceptor customerInterceptor;
 
     public void addInterceptors(InterceptorRegistry registry) {
- //车主端拦截器
+        //车主端拦截器
         registry.addInterceptor(customerInterceptor).addPathPatterns("/mini/c/**");
- //B端拦截器
+        //B端拦截器
         registry.addInterceptor(userInterceptor)
                 .addPathPatterns("/card/**", "/marketing/**", "/mini/**", "/finance/**", "/distribution/**", "/order/reservation/**", "/customer/group/**")
                 .excludePathPatterns("/feign/**", "/**/client/detail");
