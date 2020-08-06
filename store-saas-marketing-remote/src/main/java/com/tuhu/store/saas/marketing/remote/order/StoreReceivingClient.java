36420d4bbcc65e6dc2343cba7992ae2bccbe0b83
wangyuqing <wangyuqing@tuhu.cn>
2020-08-06 10:26:22 +0800

    修改配置文件

--- store-saas-marketing-remote/src/main/java/com/tuhu/store/saas/marketing/remote/order/StoreReceivingClient.java
+++ store-saas-marketing-remote/src/main/java/com/tuhu/store/saas/marketing/remote/order/StoreReceivingClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "${feign.application.jvstore.order.name}",fallbackFactory = StoreReceivingRemoteFactory.class)
+@FeignClient(name = "${feign.application.orderServer.name}",fallbackFactory = StoreReceivingRemoteFactory.class)
 public interface StoreReceivingClient {
 
     /**
