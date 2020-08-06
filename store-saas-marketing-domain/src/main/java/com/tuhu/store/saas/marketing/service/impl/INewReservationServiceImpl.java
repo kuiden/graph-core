66e5e3f26c6848cf9f076b3110aa86b5c2ead000
yanglanqing <yanglanqing@tuhu.cn>
2020-08-06 17:51:32 +0800

    查询C端预约单列表

--- store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/INewReservationServiceImpl.java
+++ store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/INewReservationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tuhu.store.saas.marketing.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
 import com.tuhu.boot.common.facade.BizBaseResponse;
 import com.tuhu.java.common.utils.DateUtil;
 import com.tuhu.store.saas.marketing.context.UserContextHolder;
@@ -16,12 +17,13 @@ import com.tuhu.store.saas.marketing.remote.request.BaseIdReqVO;
 import com.tuhu.store.saas.marketing.remote.request.CustomerReq;
 import com.tuhu.store.saas.marketing.remote.request.StoreInfoVO;
 import com.tuhu.store.saas.marketing.remote.storeuser.StoreUserClient;
+import com.tuhu.store.saas.marketing.request.CReservationListReq;
 import com.tuhu.store.saas.marketing.request.NewReservationReq;
 import com.tuhu.store.saas.marketing.request.ReservePeriodReq;
 import com.tuhu.store.saas.marketing.response.ReservationPeriodResp;
+import com.tuhu.store.saas.marketing.response.dto.ReservationDTO;
 import com.tuhu.store.saas.marketing.service.INewReservationService;
 import com.tuhu.store.saas.marketing.service.IReservationOrderService;
-import com.tuhu.store.saas.marketing.util.DateUtils;
 import com.tuhu.store.saas.marketing.util.IdKeyGen;
 import com.tuhu.store.saas.marketing.util.KeyResult;
 import com.tuhu.store.saas.marketing.util.StoreRedisUtils;
@@ -140,19 +142,56 @@ public class INewReservationServiceImpl implements INewReservationService {
         return reservationOrderService.update(newOrder) > 0;
     }
 
+    @Override
+    public PageInfo<ReservationDTO> getCReservationList(CReservationListReq req) {
+        PageInfo<ReservationDTO> result = new PageInfo<>();
+        List<ReservationDTO> list = new ArrayList<>();
+        List<SrvReservationOrder> daoList = reservationOrderMapper.getCReservationList(req.getStoreId(),req.getCustomerId(),req.getPageNum(),req.getPageSize());
+        if(CollectionUtils.isNotEmpty(daoList)){
+            for(SrvReservationOrder dao : daoList){
+                ReservationDTO dto = new ReservationDTO();
+                dto.setReservationTime(dao.getEstimatedArriveTime().getTime());
+                dto.setStatus(transCStatus(dao));
+                list.add(dto);
+            }
+        }
+        result.setList(list);
+        result.setTotal(reservationOrderMapper.getCReservationCount(req.getStoreId(),req.getCustomerId()));
+        return result;
+    }
+
+    //C端预约列表的已预约对应B端的待确认和已确认
+    private String transCStatus(SrvReservationOrder order){
+        String result = order.getStatus();
+        if(SrvReservationStatusEnum.UNCONFIRMED.getEnumCode().equals(order.getStatus()) || SrvReservationStatusEnum.CONFIRMED.getEnumCode().equals(order.getStatus())){
+             result = "ORDERED";
+        }
+        return result;
+    }
+
     /**
      * 新增和修改预约单共同的校验
      * @param req
      * @param type 门店：0,小程序：1,H5:2
      */
-    private void validReservationParam(NewReservationReq req, Integer type){
+    private void validReservationParam(NewReservationReq req, Integer type) {
         //校验预约的时间
         if (req.getEstimatedArriveTime().compareTo(new Date()) < 0) {
             throw new StoreSaasMarketingException("到店时间需大于当前时间");
         }
         Map<String,Date> storeMap = getStoreInfo(req.getStoreId());
-        if (DateUtils.getHourOfDay(req.getEstimatedArriveTime()) < DateUtils.getHourOfDay(storeMap.get("startTime"))
-                || DateUtils.getHourOfDay(req.getEstimatedArriveTime()) > DateUtils.getHourOfDay(storeMap.get("endTime"))) {
+        Calendar arriveTime = Calendar.getInstance();
+        Calendar startTime = Calendar.getInstance();
+        Calendar endTime = Calendar.getInstance();
+        try{
+            arriveTime.setTime(hmDateFormat.parse(hmDateFormat.format(req.getEstimatedArriveTime())));
+            startTime.setTime(storeMap.get("startTime"));
+            endTime.setTime(storeMap.get("endTime"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if (arriveTime.before(startTime)
+                || arriveTime.after(endTime)) {
             throw new StoreSaasMarketingException("当前预约时间段不能预约,门店预约时间范围为：" + hmDateFormat.format(storeMap.get("startTime")) + "-" + hmDateFormat.format(storeMap.get("endTime")));
         }
         //如果手机号不在门店客户中，添加客户(只有小程序和H5会出现这种情况)
