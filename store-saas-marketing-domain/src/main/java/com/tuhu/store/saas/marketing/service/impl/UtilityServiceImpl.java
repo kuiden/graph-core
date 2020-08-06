7820bcadf61ee1bb189a2a425a111109dc02eca2
zhuzhoutong <zhuzhoutong@tuhu.cn>
2020-08-06 15:12:28 +0800

    【新增】短链接口url编码

--- store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/UtilityServiceImpl.java
+++ store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/UtilityServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tuhu.store.saas.marketing.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.mengfan.common.util.GatewayClient;
 import com.tuhu.store.saas.marketing.service.IUtilityService;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -34,7 +36,7 @@ public class UtilityServiceImpl implements IUtilityService {
             HttpHeaders headers = new HttpHeaders();
             headers.add("RequestID", UUID.randomUUID().toString());
             Map<String,Object> params = new HashMap<>();
-            params.put("uri",longUrl);
+            params.put("uri", URLEncoder.encode(longUrl, "UTF-8"));
             params.put("source","store_saas");
             Map<String,Object> result = gatewayClient.getForObject(alrAccessoryProductUrl+"/Utility/Utility/GetTuhuDwz",params,headers,Map.class);
             log.info("获取短链返回值：{}", result);
