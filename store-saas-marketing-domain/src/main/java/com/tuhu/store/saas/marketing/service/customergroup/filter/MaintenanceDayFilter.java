dada97d8dbf68f4d7e03d35cb5641b96236dcfba
sujie <sujie@tuhu.cn>
2020-08-07 15:21:27 +0800

    客群-保养规则计算

--- store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/customergroup/filter/MaintenanceDayFilter.java
+++ store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/customergroup/filter/MaintenanceDayFilter.java
@@ -1,12 +1,20 @@
 package com.tuhu.store.saas.marketing.service.customergroup.filter;
 
+import com.google.common.collect.Lists;
+import com.tuhu.boot.common.facade.BizBaseResponse;
+import com.tuhu.store.saas.crm.dto.CustomerDTO;
 import com.tuhu.store.saas.crm.vo.VehicleMaintenanceVo;
 import com.tuhu.store.saas.marketing.remote.crm.CustomerClient;
 import com.tuhu.store.saas.marketing.service.customergroup.AbstractFactorFilter;
+import com.tuhu.store.saas.marketing.util.DateUtils;
 import com.tuhu.store.saas.marketing.util.SpringApplicationContextUtil;
+import com.tuhu.store.saas.order.response.serviceorder.ListCustomerInfoResp;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,12 +39,31 @@ public class MaintenanceDayFilter extends AbstractFactorFilter {
 
     @Override
     public List<String> filterSelf() {
-
         CustomerClient customerClient = SpringApplicationContextUtil.getBean(CustomerClient.class);
         VehicleMaintenanceVo vehicleMaintenanceVo = new VehicleMaintenanceVo();
         vehicleMaintenanceVo.setTenantId(tenantId);
         vehicleMaintenanceVo.setStoreId(storeId);
-        customerClient.getCustomerByVehicleMaintenance(vehicleMaintenanceVo);
-        return null;
+        vehicleMaintenanceVo.setNextMaintenanceDateStart(geStartDateAfterAdd(dayStart));
+        vehicleMaintenanceVo.setNextMaintenanceDateEnd(geEndDateAfterAdd(dayEnd));
+        List<CustomerDTO> CustomerDTOList = customerClient.getCustomerByVehicleMaintenance(vehicleMaintenanceVo).getData();
+        List<String> customerIdList = Lists.newArrayList();
+        if(CollectionUtils.isNotEmpty(CustomerDTOList)){
+            for(CustomerDTO customerDto : CustomerDTOList){
+                if(StringUtils.isNotBlank(customerDto.getId()) && !customerIdList.contains(customerDto.getId())){
+                    customerIdList.add(customerDto.getId());
+                }
+            }
+        }
+        return customerIdList;
+    }
+
+    private Date geStartDateAfterAdd(Integer dayLength){
+        Date dayBegain = DateUtils.getDayBegin();
+        return DateUtils.getNextDay(dayBegain,dayLength);
+    }
+
+    private Date geEndDateAfterAdd(Integer dayLength){
+        Date dayEnd = DateUtils.getDayEnd();
+        return DateUtils.getNextDay(dayEnd,dayLength);
     }
 }
