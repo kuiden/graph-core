83a8fbaaca67f13996553b88b96db39489edda1c
kudeng <kudeng@tuhu.cn>
2020-08-13 19:48:00 +0800

    营销记录用户足迹接口修改

--- store-saas-marketing-api/src/main/java/com/tuhu/store/saas/marketing/controller/auth/RecordApi.java
+++ store-saas-marketing-api/src/main/java/com/tuhu/store/saas/marketing/controller/auth/RecordApi.java
@@ -6,6 +6,7 @@ import com.tuhu.store.saas.marketing.request.ClientEventRecordRequest;
 import com.tuhu.store.saas.marketing.service.IClientEventRecordService;
 import com.tuhu.store.saas.user.vo.EventTypeEnum;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -27,14 +28,16 @@ public class RecordApi {
     private IClientEventRecordService iClientEventRecordService;
 
     @GetMapping(value = "/user/{contentType}/record")
+    @ApiOperation(value = "门店saas营销用户足迹记录")
     public BizBaseResponse recordEndUserEvent(@PathVariable String contentType, @NotNull @Validated ClientEventRecordRequest clientEventRecordRequest) {
         clientEventRecordRequest.setContentType(contentType);
-        clientEventRecordRequest.setEventType(EventTypeEnum.VISIT.getCode());
+//        clientEventRecordRequest.setEventType(EventTypeEnum.VISIT.getCode());
         iClientEventRecordService.recordEndUserEvent(clientEventRecordRequest);
         return BizBaseResponse.success();
     }
 
     @GetMapping(value = "/user/{contentType}/forward")
+    @ApiOperation(value = "门店saas营销用户转发记录")
     public BizBaseResponse recordEndUserForwardEvent(@PathVariable String contentType, @NotNull @Validated ClientEventRecordRequest clientEventRecordRequest) {
         clientEventRecordRequest.setContentType(contentType);
         clientEventRecordRequest.setEventType(EventTypeEnum.WECHATFORWARD.getCode());
