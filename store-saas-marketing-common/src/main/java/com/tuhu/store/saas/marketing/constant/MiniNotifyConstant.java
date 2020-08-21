89b262fbb9244a6ea668c0cf4ef08f5115ca1826
wangxiang2 <wangxiang2@tuhu.cn>
2020-08-21 17:24:18 +0800

    预约消息，添加地址信息

---
+++ store-saas-marketing-common/src/main/java/com/tuhu/store/saas/marketing/constant/MiniNotifyConstant.java
@@ -0,0 +1,32 @@
+package com.tuhu.store.saas.marketing.constant;
+
+public class MiniNotifyConstant {
+
+    /**
+     * 预约的服务门店
+     */
+    public static final String STORENAME = "预约的服务门店";
+    /**
+     * 预约到店时间
+     */
+    public static final String ESTIMATEDARRIVETIME = "2020-10-01 10:00:00";
+
+    /**
+     * 预约到店时间
+     */
+    public static final String ADDRESS = "请到预约门店地址";
+
+
+    /**
+     * 预约人电话
+     */
+    public static final String PHONENUMBER = "18804126666";
+
+    /**
+     * 预约备注
+     */
+    public static final String DESCRIPTION = "进站请出示此页面！";
+
+
+
+}
