281612ce24135d03b57bfa6cf568973a0f419d46
sujie <sujie@tuhu.cn>
2020-08-21 15:29:48 +0800

    计算客群人数的时，修复更改tenantID的问题

--- store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/CustomerGroupServiceImpl.java
+++ store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/CustomerGroupServiceImpl.java
@@ -671,7 +671,7 @@ public class CustomerGroupServiceImpl implements ICustomerGroupService {
                 List<String> singleCustomerIdList = CustomerGroupFilterFactory.createFilter(customerGroupDto).filterProcess();
                 StoreCustomerGroupRelation record = new StoreCustomerGroupRelation();
                 record.setId(customerGroupDto.getId());
-                record.setTenantId(req.getTenantId());
+                //record.setTenantId(req.getTenantId());
                 if(CollectionUtils.isNotEmpty(singleCustomerIdList)) {
                     record.setCustomerCount(Long.valueOf(singleCustomerIdList.size()));
                     amap.put(customerGroupDto.getId().toString(), singleCustomerIdList);
