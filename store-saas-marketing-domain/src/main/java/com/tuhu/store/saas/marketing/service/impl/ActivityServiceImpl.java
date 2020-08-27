fa30931f553f263a9737ad4d98de69e0925f8be0
kudeng <kudeng@tuhu.cn>
2020-08-27 10:42:07 +0800

    营销活动报名短信问题修改

--- store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/ActivityServiceImpl.java
+++ store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/impl/ActivityServiceImpl.java
@@ -20,10 +20,7 @@ import com.tuhu.store.saas.dto.product.ServiceGoodDTO;
 import com.tuhu.store.saas.marketing.context.EndUserContextHolder;
 import com.tuhu.store.saas.marketing.context.UserContextHolder;
 import com.tuhu.store.saas.marketing.dataobject.Customer;
-import com.tuhu.store.saas.marketing.enums.CrmReturnCodeEnum;
-import com.tuhu.store.saas.marketing.enums.EventTypeEnum;
-import com.tuhu.store.saas.marketing.enums.MarketingBizErrorCodeEnum;
-import com.tuhu.store.saas.marketing.enums.MarketingCustomerUseStatusEnum;
+import com.tuhu.store.saas.marketing.enums.*;
 import com.tuhu.store.saas.marketing.exception.MarketingException;
 import com.tuhu.store.saas.marketing.mysql.marketing.write.dao.ActivityCustomerMapper;
 import com.tuhu.store.saas.marketing.mysql.marketing.write.dao.ActivityItemMapper;
@@ -1091,6 +1088,7 @@ public class ActivityServiceImpl implements IActivityService {
         CustomerAndVehicleReq customerAndVehicleReq = new CustomerAndVehicleReq();
         customerAndVehicleReq.setCustomerId(activityCustomer.getCustomerId());
         sendRemindReq.setCustomerList(Collections.singletonList(customerAndVehicleReq));
+        sendRemindReq.setSource(SMSTypeEnum.SAAS_STORE_ACTIVITY_APPLY.templateCode());
         sendRemindReq.setMessageTemplateId(applyMessageTemplateId);
         List<String> datas = new ArrayList<>();
         datas.add(activity.getActivityTitle());
@@ -1191,6 +1189,7 @@ public class ActivityServiceImpl implements IActivityService {
         customerAndVehicleReq.setCustomerId(activityApplyReq.getCustomerId());
         sendRemindReq.setCustomerList(Collections.singletonList(customerAndVehicleReq));
         sendRemindReq.setMessageTemplateId(applyMessageTemplateId);
+        sendRemindReq.setSource(SMSTypeEnum.SAAS_STORE_ACTIVITY_APPLY.templateCode());
         List<String> datas = new ArrayList<>();
         datas.add(activity.getActivityTitle());
         String datePattern = "yyyy年MM月dd日";
