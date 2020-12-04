f5ef670e0dfc08c7650377daedbb8afc452f4859
yangshengyong <yangshengyong@tuhu.cn>
2020-12-04 10:07:56 +0800

    秒杀活动pc

--- store-saas-marketing-api/src/main/java/com/tuhu/store/saas/marketing/controller/seckill/SeckillActivityPcController.java
+++ store-saas-marketing-api/src/main/java/com/tuhu/store/saas/marketing/controller/seckill/SeckillActivityPcController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/feign/seckill/activity")
-public class SeckillActivityMinController extends BaseApi {
+public class SeckillActivityPcController extends BaseApi {
 
 }
 
