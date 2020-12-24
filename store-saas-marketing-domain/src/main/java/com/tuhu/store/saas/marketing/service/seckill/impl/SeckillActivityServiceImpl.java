a020a4365c83fdba42a8394f598bcf86adaa6362
chaoyuan <chaoyuan@tuhu.cn>
2020-12-24 16:32:31 +0800

    活动报错流程修改

--- store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/seckill/impl/SeckillActivityServiceImpl.java
+++ store-saas-marketing-domain/src/main/java/com/tuhu/store/saas/marketing/service/seckill/impl/SeckillActivityServiceImpl.java
@@ -222,7 +222,7 @@ public class SeckillActivityServiceImpl extends ServiceImpl<SeckillActivityMappe
         } else {
             //删除关联活动的商品和服务item
             itemService.deleteBatchIds(model.getItems().stream().map(x->x.getId()).collect(Collectors.toList()));
-             if(super.updateById(entity)){
+            if (this.baseMapper.updateByPrimaryKey(entity) > 0) {
                  result = entity.getId();
                  insertSeckillActivityItemFunc.apply(model);
                  saveFuncAttachedInfoFunc.apply(model);
