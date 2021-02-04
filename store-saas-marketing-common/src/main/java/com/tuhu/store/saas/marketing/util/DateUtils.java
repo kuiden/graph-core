ac6c485550680aecf140b4292306aeb6a2444f35
baoyongying <baoyongying@tuhu.cn>
2021-02-04 20:16:55 +0800

    去掉DateUtils 获取当天结束时间去掉999MILLISECOND

--- store-saas-marketing-common/src/main/java/com/tuhu/store/saas/marketing/util/DateUtils.java
+++ store-saas-marketing-common/src/main/java/com/tuhu/store/saas/marketing/util/DateUtils.java
@@ -62,7 +62,7 @@ public class DateUtils {
         calendar.set(Calendar.HOUR_OF_DAY, 23);
         calendar.set(Calendar.MINUTE, 59);
         calendar.set(Calendar.SECOND, 59);
-        calendar.set(Calendar.MILLISECOND, 999);
+        //calendar.set(Calendar.MILLISECOND, 999);
         return calendar.getTime();
     }
 
